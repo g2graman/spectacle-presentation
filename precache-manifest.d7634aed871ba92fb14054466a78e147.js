88976beaf8533c193dbab5b409a920b81ec60d46
Francesco Gramano <fran.gramano@mail.utoronto.ca>
2019-05-28 11:05:22 -0400

    Updates

---
+++ precache-manifest.d7634aed871ba92fb14054466a78e147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3db32351413f5ac25b7d",
+    "url": "/spectacle-presentation/static/js/runtime~main.3db32351.js"
+  },
+  {
+    "revision": "8c1333589c90db8c9907",
+    "url": "/spectacle-presentation/static/js/main.8c133358.chunk.js"
+  },
+  {
+    "revision": "9236b452354f222e9fcc",
+    "url": "/spectacle-presentation/static/js/2.9236b452.chunk.js"
+  },
+  {
+    "revision": "8c1333589c90db8c9907",
+    "url": "/spectacle-presentation/static/css/main.22bd18cf.chunk.css"
+  },
+  {
+    "revision": "539ef8d488578bcdc808eb6a1ab76e35",
+    "url": "/spectacle-presentation/index.html"
+  }
+];
